11161a717b149465d1d2be687fdd3e89385c56c2
kcelik <kcelik.istanbul@gmail.com>
2018-11-13 16:46:56 +0300

    Update Sample 07 Insert Update Detele.sql

--- Sample 07 Insert Update Detele.sql
+++ Sample 07 Insert Update Detele.sql
@@ -1,101 +1,91 @@
 -- Create a table for the demo
-CREATE TABLE SalesLT.CallLog
+DROP TABLE Sales.CallLog;
+CREATE TABLE Sales.CallLog
 (
 	CallID int IDENTITY PRIMARY KEY NOT NULL,
 	CallTime datetime NOT NULL DEFAULT GETDATE(),
 	SalesPerson nvarchar(256) NOT NULL,
-	CustomerID int NOT NULL REFERENCES SalesLT.Customer(CustomerID),
+	CustomerID int NOT NULL REFERENCES Sales.Customer(CustomerID),
 	PhoneNumber nvarchar(25) NOT NULL,
-	Notes nvarchar(max) NULL
+	Notes nvarchar(max) NULL,
 );
 GO
 
 -- Insert a row
-INSERT INTO SalesLT.CallLog
-VALUES
-('2015-01-01T12:30:00', 'adventure-works\pamela0', 1, '245-555-0173', 'Returning call re: enquiry about delivery');
+INSERT INTO Sales.CallLog VALUES ('2015-01-01T12:30:00', 'adventure-works\pamela0', 1, '245-555-0173', 'Returning call re: enquiry about delivery');
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Insert defaults and nulls
-INSERT INTO SalesLT.CallLog
-VALUES
-(DEFAULT, 'adventure-works\david8', 2, '170-555-0127', NULL);
+INSERT INTO Sales.CallLog VALUES (DEFAULT, 'adventure-works\david8', 2, '170-555-0127', NULL);
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Insert a row with explicit columns
-INSERT INTO SalesLT.CallLog (SalesPerson, CustomerID, PhoneNumber)
-VALUES
-('adventure-works\jillian0', 3, '279-555-0130');
+INSERT INTO Sales.CallLog (SalesPerson, CustomerID, PhoneNumber) VALUES ('adventure-works\jillian0', 3, '279-555-0130');
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Insert multiple rows
-INSERT INTO SalesLT.CallLog
-VALUES
-(DATEADD(mi,-2, GETDATE()), 'adventure-works\jillian0', 4, '710-555-0173', NULL),
+INSERT INTO Sales.CallLog VALUES(DATEADD(mi,-2, GETDATE()), 'adventure-works\jillian0', 4, '710-555-0173', NULL),
 (DEFAULT, 'adventure-works\shu0', 5, '828-555-0186', 'Called to arrange deliver of order 10987');
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Insert the results of a query
-INSERT INTO SalesLT.CallLog (SalesPerson, CustomerID, PhoneNumber, Notes)
-SELECT SalesPerson, CustomerID, Phone, 'Sales promotion call'
-FROM SalesLT.Customer
-WHERE CompanyName = 'Big-Time Bike Store';
+INSERT INTO Sales.CallLog (SalesPerson, CustomerID, PhoneNumber, Notes)
+SELECT p.FirstName+ ' '+p.LastName , CustomerID, pp.PhoneNumber, 'Sales promotion call'
+FROM [Sales].[SalesPerson] sp 
+INNER JOIN [Sales].[SalesOrderHeader] soh ON sp.[BusinessEntityID] = soh.[SalesPersonID]
+INNER JOIN [Person].[Person] p ON p.[BusinessEntityID] = sp.[BusinessEntityID]
+LEFT OUTER JOIN [Person].[PersonPhone] pp ON pp.[BusinessEntityID] = p.[BusinessEntityID]
+WHERE pp.PhoneNumber IS NOT NULL;
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Retrieving inserted identity
-INSERT INTO SalesLT.CallLog (SalesPerson, CustomerID, PhoneNumber)
-VALUES
-('adventure-works\josé1', 10, '150-555-0127');
-
+INSERT INTO Sales.CallLog (SalesPerson, CustomerID, PhoneNumber) VALUES ('adventure-works\josÃ©1', 10, '150-555-0127');
 SELECT SCOPE_IDENTITY();
-
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 --Overriding Identity
-SET IDENTITY_INSERT SalesLT.CallLog ON;
-
-INSERT INTO SalesLT.CallLog (CallID, SalesPerson, CustomerID, PhoneNumber)
-VALUES
-(9, 'adventure-works\josé1', 11, '926-555-0159');
-
-SET IDENTITY_INSERT SalesLT.CallLog OFF;
-
-SELECT * FROM SalesLT.CallLog;
+SET IDENTITY_INSERT Sales.CallLog ON;
+INSERT INTO Sales.CallLog (CallID, SalesPerson, CustomerID, PhoneNumber) VALUES (9, 'adventure-works\josÃ©1', 11, '926-555-0159');
+SET IDENTITY_INSERT Sales.CallLog OFF;
+SELECT * FROM Sales.CallLog;
 
 
 -- Update a table
-UPDATE SalesLT.CallLog
-SET Notes = 'No notes'
+UPDATE Sales.CallLog  
+SET Notes = 'No notes' 
 WHERE Notes IS NULL;
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Update multiple columns
-UPDATE SalesLT.CallLog
+UPDATE Sales.CallLog
 SET SalesPerson = '', PhoneNumber = ''
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Update from results of a query
-UPDATE SalesLT.CallLog
+UPDATE Sales.CallLog
 SET SalesPerson = c.SalesPerson, PhoneNumber = c.Phone
-FROM SalesLT.Customer AS c
-WHERE c.CustomerID = SalesLT.CallLog.CustomerID;
+FROM [Sales].[SalesPerson] sp 
+INNER JOIN [Sales].[SalesOrderHeader] soh ON sp.[BusinessEntityID] = soh.[SalesPersonID]
+INNER JOIN [Person].[Person] p ON p.[BusinessEntityID] = sp.[BusinessEntityID]
+LEFT OUTER JOIN [Person].[PersonPhone] pp ON pp.[BusinessEntityID] = p.[BusinessEntityID]
+WHERE soh.CustomerID = Sales.CallLog.CustomerID;
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Delete rows
-DELETE FROM SalesLT.CallLog
+DELETE FROM Sales.CallLog
 WHERE CallTime < DATEADD(dd, -7, GETDATE());
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
 
 -- Truncate the table
-TRUNCATE TABLE SalesLT.CallLog;
+TRUNCATE TABLE Sales.CallLog;
 
-SELECT * FROM SalesLT.CallLog;
+SELECT * FROM Sales.CallLog;
