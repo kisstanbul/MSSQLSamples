e411e2641c65f2a03878cf0497b2dad8236afafb
kcelik <kcelik.istanbul@gmail.com>
2018-11-13 16:30:41 +0300

    Update Sample 06 Grouping.sql

--- Sample 06 Grouping.sql
+++ Sample 06 Grouping.sql
@@ -1,88 +1,149 @@
 ----------------------------------------------------------------------------------
 -- Grouping
 ----------------------------------------------------------------------------------
-SELECT cat.ParentProductCategoryName, cat.ProductCategoryName, count(prd.ProductID) AS Products
-FROM SalesLT.vGetAllCategories as cat
-LEFT JOIN SalesLT.Product AS prd
-ON prd.ProductCategoryID = cat.ProductcategoryID
-GROUP BY cat.ParentProductCategoryName, cat.ProductCategoryName
---GROUP BY GROUPING SETS(cat.ParentProductCategoryName, cat.ProductCategoryName, ())
---GROUP BY ROLLUP (cat.ParentProductCategoryName, cat.ProductCategoryName)
---GROUP BY CUBE (cat.ParentProductCategoryName, cat.ProductCategoryName)
-ORDER BY cat.ParentProductCategoryName, cat.ProductCategoryName;
--- CUBE provides shortcut for defining grouping sets given a list of columns
-SELECT Status, CustomerID, SUM(TotalDue) AS TotalAmountDue
-FROM SalesLT.SalesOrderHeader
-GROUP BY CUBE(Status, CustomerID)
-ORDER BY Status, CustomerID;
--- ROLLUP provides shortcut for defining grouping sets, creates combinations assuming input columns form a hierarchy
-SELECT Status, CustomerID, SUM(TotalDue) AS TotalAmountDue
-FROM SalesLT.SalesOrderHeader
-GROUP BY ROLLUP(Status, CustomerID)
-ORDER BY Status, CustomerID;
+-- A) Use a simple GROUP BY clause
+SELECT SalesOrderID,
+       SUM(LineTotal) AS SubTotal
+FROM Sales.SalesOrderDetail AS sod
+GROUP BY SalesOrderID
+ORDER BY SalesOrderID;  
+-- B) Use a GROUP BY clause with multiple tables
+SELECT a.City,
+       COUNT(bea.AddressID) EmployeeCount
+FROM Person.BusinessEntityAddress AS bea
+     INNER JOIN Person.Address AS a ON bea.AddressID = a.AddressID
+GROUP BY a.City
+ORDER BY a.City;  
+-- C) Use a GROUP BY clause with a HAVING clause
+SELECT DATEPART(yyyy, OrderDate) AS N'Year',
+       SUM(TotalDue) AS N'Total Order Amount'
+FROM Sales.SalesOrderHeader
+GROUP BY DATEPART(yyyy, OrderDate)
+HAVING DATEPART(yyyy, OrderDate) >= N'2003'
+ORDER BY DATEPART(yyyy, OrderDate); 
+--
 
+CREATE TABLE Sales
+(Country VARCHAR(50),
+ Region  VARCHAR(50),
+ Sales   INT
+);
+INSERT INTO sales
+VALUES
+(N'Canada',
+ N'Alberta',
+ 100
+);
+INSERT INTO sales
+VALUES
+(N'Canada',
+ N'British Columbia',
+ 200
+);
+INSERT INTO sales
+VALUES
+(N'Canada',
+ N'British Columbia',
+ 300
+);
+INSERT INTO sales
+VALUES
+(N'United States',
+ N'Montana',
+ 100
+);
+--
+SELECT Country,
+       Region,
+       SUM(sales) AS TotalSales
+FROM Sales
+GROUP BY Country,
+         Region;
+--
+SELECT Country,
+       Region,
+       SUM(Sales) AS TotalSales
+FROM Sales
+GROUP BY ROLLUP(Country, Region);
+--
+SELECT Country,
+       Region,
+       SUM(Sales) AS TotalSales
+FROM Sales
+GROUP BY CUBE(Country, Region);
+--
+SELECT Country,
+       Region,
+       SUM(Sales) AS TotalSales
+FROM Sales
+GROUP BY GROUPING SETS(ROLLUP(Country, Region), CUBE(Country, Region));
+--
 ----------------------------------------------------------------------------------
 -- Pivot
 ----------------------------------------------------------------------------------
-SELECT * FROM
-(SELECT P.ProductID, PC.Name,ISNULL(P.Color, 'Uncolored') AS Color
- FROM saleslt.productcategory AS PC
- JOIN SalesLT.Product AS P
- ON PC.ProductCategoryID=P.ProductCategoryID
- ) AS PPC
-PIVOT(COUNT(ProductID) FOR Color IN([Red],[Blue],[Black],[Silver],[Yellow],[Grey], [Multi], [Uncolored])) as pvt
-ORDER BY Name;
-
--- Unpivot
-CREATE TABLE #ProductColorPivot
-(Name varchar(50), Red int, Blue int, Black int, Silver int, Yellow int, Grey int , multi int, uncolored int);
-
-INSERT INTO #ProductColorPivot
-SELECT * FROM
-(SELECT P.ProductID, PC.Name,ISNULL(P.Color, 'Uncolored') AS Color
- FROM saleslt.productcategory AS PC
- JOIN SalesLT.Product AS P
- ON PC.ProductCategoryID=P.ProductCategoryID
- ) AS PPC
-PIVOT(COUNT(ProductID) FOR Color IN([Red],[Blue],[Black],[Silver],[Yellow],[Grey], [Multi], [Uncolored])) as pvt
-ORDER BY Name;
-
-SELECT Name, Color, ProductCount
+-- Select 
+SELECT PS.Name,
+       P.Color,
+       PIn.Quantity
+FROM Production.Product P
+     INNER JOIN Production.ProductSubcategory PS ON PS.ProductSubcategoryID = P.ProductSubcategoryID
+     LEFT JOIN Production.ProductInventory PIn ON P.ProductID = PIn.ProductID;
+--Pivot
+SELECT *
 FROM
-(SELECT Name,
-[Red],[Blue],[Black],[Silver],[Yellow],[Grey], [Multi], [Uncolored]
-FROM #ProductColorPivot) pcp
-UNPIVOT
-(ProductCount FOR Color IN ([Red],[Blue],[Black],[Silver],[Yellow],[Grey], [Multi], [Uncolored])
-) AS ProductCounts
-
-
-
-
-
-
-
--- Unpivot
-CREATE TABLE #SalesByQuarter
-(ProductID int,
- Q1 money,
- Q2 money,
- Q3 money,
- Q4 money);
-
-INSERT INTO #SalesByQuarter
-VALUES
-(1, 19999.00, 21567.00, 23340.00, 25876.00),
-(2, 10997.00, 12465.00, 13367.00, 14365.00),
-(3, 21900.00, 21999.00, 23376.00, 23676.00);
-
-SELECT * FROM #SalesByQuarter;
-
-SELECT ProductID, Period, Revenue
+(
+    SELECT PS.Name,
+           P.Color,
+           PIn.Quantity
+    FROM Production.Product P
+         INNER JOIN Production.ProductSubcategory PS ON PS.ProductSubcategoryID = P.ProductSubcategoryID
+         LEFT JOIN Production.ProductInventory PIn ON P.ProductID = PIn.ProductID
+) DataTable PIVOT(SUM(Quantity) FOR Color IN(Black,
+                                             Blue,
+                                             Grey,
+                                             Multi,
+                                             Red,
+                                             Silver,
+                                             [Silver/Black],
+                                             White,
+                                             Yellow)) PivotTable;
+-- Pivot2
+SELECT *
 FROM
-(SELECT ProductID,
-Q1, Q2, Q3, Q4
-FROM #SalesByQuarter) sbq
+(
+    SELECT YEAR(OrderDate) [Year],
+           MONTH(OrderDate) [Month],
+           SubTotal
+    FROM Sales.SalesOrderHeader
+) TableDate PIVOT(SUM(SubTotal) FOR [Month] IN([1],
+                                               [2],
+                                               [3],
+                                               [4],
+                                               [5],
+                                               [6],
+                                               [7],
+                                               [8],
+                                               [9],
+                                               [10],
+                                               [11],
+                                               [12])) PivotTable;
+-- UnPivot
+CREATE TABLE pvt (VendorID int, Emp1 int, Emp2 int,
+Emp3 int, Emp4 int, Emp5 int)
+GO
+INSERT INTO pvt VALUES (1,4,3,5,4,4)
+INSERT INTO pvt VALUES (2,4,1,5,5,5)
+INSERT INTO pvt VALUES (3,4,3,5,4,4)
+INSERT INTO pvt VALUES (4,4,2,5,5,4)
+INSERT INTO pvt VALUES (5,5,1,5,5,5)
+GO
+--Unpivot the table.
+SELECT VendorID, Employee, Orders
+FROM
+(SELECT VendorID, Emp1, Emp2, Emp3, Emp4, Emp5
+FROM pvt) p
 UNPIVOT
-(Revenue FOR Period IN (Q1, Q2, Q3, Q4)
-) AS RevenueReport
+(Orders FOR Employee IN
+(Emp1, Emp2, Emp3, Emp4, Emp5)
+)AS unpvt
+GO
